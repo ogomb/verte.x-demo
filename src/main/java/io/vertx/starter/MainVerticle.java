2c49f4b8b84d0b0f212743fcd8a4644e3182a11a
ogomb <lewis.mbogo@andela.com>
2019-05-06 12:32:10 +0300

    database initialization.

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -2,11 +2,26 @@ package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key,Name varchar(255) unique, Content clob)";
+  private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+  private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+  private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+  private static final String SQL_ALL_PAGES = "select Name from Pages";
+  private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
+  private JDBCClient dbClient;
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
   @Override
   public void start(Future<Void> startFuture) throws Exception {
+    //related async operations composed.
     Future steps = prepareDatabase().compose(v-> startHttpServer());
     steps.setHandler(startFuture.completer());
   }
@@ -14,6 +29,30 @@ public class MainVerticle extends AbstractVerticle {
 
   private Future prepareDatabase(){
     Future future = Future.future();
+    // creates a shared connection to be shared among verticles known to the vertex instance
+    // JDBC client connection is made by passing vertx JSON object.
+    dbClient = JDBCClient.createShared(vertx, new JsonObject()
+      .put("url", "jdbc:hsqldb:file:db/wiki")
+      .put("driver_class", "org.hsqldb.jdbcDriver")
+      .put("max_pool_size", 30));
+
+    dbClient.getConnection(asyncResult -> {
+      if (asyncResult.failed()){
+        LOGGER.error("Could not open a database connection", asyncResult.cause());
+        future.fail(asyncResult.cause());
+      } else {
+        SQLConnection connection = asyncResult.result();
+        connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+          connection.close();
+          if (create.failed()){
+            LOGGER.error("Database preparation error", create.cause());
+            future.fail(create.cause());
+          } else {
+            future.complete();
+          }
+        });
+      }
+    });
     return future;
   }
 
