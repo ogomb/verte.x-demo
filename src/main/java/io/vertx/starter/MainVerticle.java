9c6a1e821b41e6520f007668917bbf6f392824fd
ogomb <lewis.mbogo@andela.com>
2019-05-06 15:22:23 +0300

    page update create/update handler.

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -82,7 +82,7 @@ public class MainVerticle extends AbstractVerticle {
     router.get("/").handler(this::indexHandler);
     router.get("/wiki/:page").handler(this::pageRenderingHandler);
     router.post().handler(BodyHandler.create());
-//    router.post("/save").handler(this::pageUpdateHandler);
+    router.post("/save").handler(this::pageUpdateHandler);
     router.post("/create").handler(this::pageCreateHandler);
 //    router.post("/delete").handler(this::pageDeletionHandler);
 
@@ -192,5 +192,39 @@ public class MainVerticle extends AbstractVerticle {
     context.response().setStatusCode(303);
     context.response().putHeader("Location", location);
     context.response().end();
+  }
+
+  private void pageUpdateHandler(RoutingContext context){
+    String id = context.request().getParam("id");
+    String title = context.request().getParam("title");
+    String markdown = context.request().getParam("markdown");
+    boolean newPage = "yes".equals(context.request().getParam("newPage"));
+
+
+    dbClient.getConnection(car -> {
+      if (car.succeeded()){
+        SQLConnection connection = car.result();
+        String sql = newPage ? SQL_CREATE_PAGE: SQL_SAVE_PAGE;
+        JsonArray params = new JsonArray();
+        if (newPage){
+          params.add(title).add(markdown);
+        } else {
+          params.add(markdown).add(id);
+        }
+
+        connection.updateWithParams(sql, params, res -> {
+          connection.close();
+          if (res.succeeded()){
+            context.response().setStatusCode(303);
+            context.response().putHeader("Location", "/wiki/" + title);
+            context.response().end();
+          } else {
+            context.fail(res.cause());
+          }
+        });
+      } else {
+        context.fail(car.cause());
+      }
+    });
   }
 }
