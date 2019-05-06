7d9bcdc5265d82ab1dd737ca6d6ff163354cf4ae
ogomb <lewis.mbogo@andela.com>
2019-05-06 12:11:18 +0300

    prepare database and then create server this are two async tasks that we compose together.

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,24 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
+  public void start(Future<Void> startFuture) throws Exception {
+    Future steps = prepareDatabase().compose(v-> startHttpServer());
+    steps.setHandler(startFuture.completer());
   }
 
+
+  private Future prepareDatabase(){
+    Future future = Future.future();
+    return future;
+  }
+
+  private Future startHttpServer(){
+    Future future = Future.future();
+    return future;
+  }
 }
