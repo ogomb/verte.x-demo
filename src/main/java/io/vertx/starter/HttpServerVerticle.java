e6ff4b78196070261e4b9971ea41bd68cc75cae3
ogomb <lewis.mbogo@andela.com>
2019-05-15 15:27:16 +0300

    create handler for the request to the api.

--- src/main/java/io/vertx/starter/HttpServerVerticle.java
+++ src/main/java/io/vertx/starter/HttpServerVerticle.java
@@ -7,11 +7,16 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 import io.vertx.starter.database.WikiDatabaseService;
@@ -35,10 +40,16 @@ public class HttpServerVerticle extends AbstractVerticle {
 
   private WikiDatabaseService dbService;
 
+  private WebClient webClient;
+
   @Override
   public void start(Future startFuture) throws Exception {
     wikiDbQueue = config().getString(CONFIG_WIKIDB_QUEUE, "wikidb.queue");
     dbService = WikiDatabaseService.createProxy(vertx, wikiDbQueue);
+
+    webClient = WebClient.create(vertx, new WebClientOptions()
+      .setSsl(true)
+      .setUserAgent("vert-x3"));
 
     HttpServer server = vertx.createHttpServer();
     Router router = Router.router(vertx);
@@ -49,6 +60,7 @@ public class HttpServerVerticle extends AbstractVerticle {
     router.post("/save").handler(this::pageUpdateHandler);
     router.post("/create").handler(this::pageCreateHandler);
     router.post("/delete").handler(this::pageDeletionHandler);
+    router.get("/backup").handler(this::backupHandler);
 
     templateEngine = FreeMarkerTemplateEngine.create(vertx);
     int portNumber = config().getInteger(CONFIG_HTTP_SERVER_PORT, 8080);
@@ -148,6 +160,57 @@ public class HttpServerVerticle extends AbstractVerticle {
         context.response().end();
       } else {
         context.fail(reply.cause());
+      }
+    });
+  }
+
+  private void backupHandler(RoutingContext context) {
+    dbService.fetchAllPagesData(reply -> {
+      if (reply.succeeded()){
+        JsonArray filesObject = new JsonArray();
+        JsonObject payload = new JsonObject()
+          .put("files", filesObject)
+          .put("language", "plaintext")
+          .put("title", "vertx-wiki-backup")
+          .put("public", true);
+
+        reply
+          .result()
+          .forEach(page -> {
+            JsonObject fileObject = new JsonObject();
+            fileObject.put("name", page.getString("NAME"));
+            fileObject.put("content", page.getString("CONTENT"));
+            filesObject.add(fileObject);
+          });
+
+        webClient.post(443, "snippets.glot.io", "/snippets")
+          .putHeader("Content-Type", "application/json")
+          .as(BodyCodec.jsonObject())
+          .sendJsonObject(payload, ar -> {
+          if (ar.succeeded()) {
+            HttpResponse<JsonObject> response = ar.result();
+            if (response.statusCode() == 200) {
+              String url = "https://glot.io/snippets/" + response.body().getString("id");
+              context.put("backup_gist_url", url);
+              indexHandler(context);
+            } else {
+              StringBuilder message = new StringBuilder()
+                .append("Could not backup the wiki: ")
+                .append(response.statusMessage());
+              JsonObject body = response.body();
+              if (body != null) {
+                message.append(System.getProperty("line.separator"))
+                  .append(body.encodePrettily());
+              }
+              LOGGER.error(message.toString());
+              context.fail(502);
+            }
+          } else {
+            Throwable err = ar.cause();
+            LOGGER.error("HTTP Client error", err);
+            context.fail(err);
+          }
+        });
       }
     });
   }
